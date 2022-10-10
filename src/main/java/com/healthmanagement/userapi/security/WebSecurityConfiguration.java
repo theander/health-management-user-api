1621021d2a216e7d8836df43590ec1c2dd41bbfe
theander <aclaudino@gmail.com>
2022-10-09 23:45:51 -0300

    Fix cors

--- src/main/java/com/healthmanagement/userapi/security/WebSecurityConfiguration.java
+++ src/main/java/com/healthmanagement/userapi/security/WebSecurityConfiguration.java
@@ -45,6 +45,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
         customAuthenticationFilter.setFilterProcessesUrl("/api/login");
+        http.cors();
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeHttpRequests().antMatchers("/api/**").permitAll();
