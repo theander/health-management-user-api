88f82d15ce01ac15e1f2b1297485c47015a21975
theander <aclaudino@gmail.com>
2023-10-01 18:17:16 -0300

    Alterado cross origin

--- src/main/java/com/healthmanagement/userapi/config/AppConfig.java
+++ src/main/java/com/healthmanagement/userapi/config/AppConfig.java
@@ -3,9 +3,11 @@ package com.healthmanagement.userapi.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class AppConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer(){
@@ -13,7 +15,7 @@ public class AppConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://54.80.8.44:3000/**")
+                        .allowedOrigins("http://54.80.8.44:3000")
                         .allowedMethods("GET","POST","DELETE","OPTIONS")
                         .allowedHeaders("authorization","contentType","Origin","Content-Type","X-Auth-Token")
                         .exposedHeaders("Authorization");
